a2a8c52e522df950a0c41312b26385fd5a95a9a1
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-09-11 12:36:52 +0100

    Restructure the core, introduce Maven3 POM file for Jenkins

--- src/test/java/org/apache/commons/httpclient/ssl/SimpleSSLSocketFactory.java
+++ src/test/java/org/apache/commons/httpclient/ssl/SimpleSSLSocketFactory.java
@@ -60,7 +60,7 @@ public class SimpleSSLSocketFactory implements SimpleSocketFactory {
     private static SSLContext createSSLContext() {
         try {
             ClassLoader cl = SimpleSocketFactory.class.getClassLoader();
-            URL url = cl.getResource("org/apache/commons/httpclient/ssl/simpleserver.keystore");
+            URL url = cl.getResource("src/main/resources/org/apache/commons/httpclient/ssl/simpleserver.keystore");
             KeyStore keystore  = KeyStore.getInstance("jks");
             InputStream is = null;
             try {
